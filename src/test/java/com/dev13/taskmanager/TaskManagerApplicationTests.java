0140b024dd87f1b19b9adfe9d836f9b54f8fb469
Vladyslav Malovanyi <sky0nf1re.bc@icloud.com>
2024-01-30 16:32:57 +0100

    add main test

---
+++ src/test/java/com/dev13/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,37 @@
+package com.dev13.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(initializers = TaskManagerApplicationTests.Initializer.class)
+@Testcontainers
+class TaskManagerApplicationTests {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:latest");
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgresContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + postgresContainer.getUsername(),
+                    "spring.datasource.password=" + postgresContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+    @Test
+    void contextLoads() {
+    }
+}
